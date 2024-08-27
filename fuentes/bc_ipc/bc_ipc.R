1f9052bf89bc01d68658fe9fa9509fc3c4a98240
cesar-medina-uv <167595695+cesar-medina-uv@users.noreply.github.com>
2024-08-26 21:07:39 -0400

    Update bc_ipc.R

--- fuentes/bc_ipc/bc_ipc.R
+++ fuentes/bc_ipc/bc_ipc.R
@@ -4,17 +4,26 @@
 
 library(readxl)
 library(tidyverse)
+#install.packages("tidyverse")
 
-ipc <- read_excel("bc_ipc/Cuadro_28032024131518.xlsx") |> 
-  rename(fecha = 1, valor = 2) |> 
-  mutate(fecha = str_extract(fecha, "\\d{4}-\\d{2}-\\d{2}"),
-         fecha = ymd(fecha)) |> 
-  filter(!is.na(fecha)) |> 
-  mutate(valor = as.numeric(valor)) |> 
-  # calcular variación mensual
-  arrange(fecha) |> 
-  mutate(variacion = valor/lag(valor)-1,
-         variacion = replace_na(variacion, 0))
+# Definir la ruta completa al archivo
+file_path <- "C:\\Users\\cesar\\OneDrive\\Documentos\\GitHub\\economia_chile\\fuentes\\bc_ipc\\Cuadro_28032024131518.xlsx"
+
+# Verificar si el archivo existe
+if (file.exists(file_path)) {
+  ipc <- read_excel(file_path) |> 
+    rename(fecha = 1, valor = 2) |> 
+    mutate(fecha = str_extract(fecha, "\\d{4}-\\d{2}-\\d{2}"),
+           fecha = ymd(fecha)) |> 
+    filter(!is.na(fecha)) |> 
+    mutate(valor = as.numeric(valor)) |> 
+    # Calcular variación mensual
+    arrange(fecha) |> 
+    mutate(variacion = valor/lag(valor)-1,
+           variacion = replace_na(variacion, 0))
+} else {
+  message("El archivo no existe en la ruta especificada: ", file_path)
+}
 
 ipc |> 
   ggplot(aes(fecha, valor)) +
@@ -24,5 +33,7 @@ ipc |>
   ggplot(aes(fecha, variacion)) +
   geom_line()
 
-  
-arrow::write_parquet(ipc, "resultados/bc_ipc.parquet")
+
+# guardar ----
+arrow::write_parquet(ipc, "C:\\Users\\cesar\\OneDrive\\Documentos\\GitHub\\economia_chile\\fuentes\\bc_ipc\\bc_ipc.parquet")
+
