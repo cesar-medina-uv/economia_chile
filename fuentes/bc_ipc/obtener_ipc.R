e9e96ad80a21aad38bc1921455d6c666af046500
cesar-medina-uv <167595695+cesar-medina-uv@users.noreply.github.com>
2024-08-26 21:07:42 -0400

    Update obtener_ipc.R

--- fuentes/bc_ipc/obtener_ipc.R
+++ fuentes/bc_ipc/obtener_ipc.R
@@ -1,8 +1,28 @@
-# https://si3.bcentral.cl/Siete/ES/Siete/Cuadro/CAP_PRECIOS/MN_CAP_PRECIOS/IPC_EMP_2023/638415285164039007
-# Precios > IPC General y medidas subyacentes, INE > Índice empalmado - 2023
+library(rvest)
+library(dplyr)
+library(janitor)
+library(tidyr)
+library(stringr)
 
+# Definir las funciones necesarias
+mes_a_numeric <- function(mes) {
+  meses <- c("Ene" = 1, "Feb" = 2, "Mar" = 3, "Abr" = 4, "May" = 5, "Jun" = 6,
+             "Jul" = 7, "Ago" = 8, "Sep" = 9, "Oct" = 10, "Nov" = 11, "Dic" = 12)
+  return(meses[mes])
+}
+
+extraer_año <- function(fecha) {
+  return(as.numeric(str_extract(fecha, "\\d{4}$")))
+}
+
+cifra_comas_a_numeric <- function(valor) {
+  return(as.numeric(gsub(",", "", valor)))
+}
+
+# URL de los datos
 url = "https://si3.bcentral.cl/Siete/ES/Siete/Cuadro/CAP_PRECIOS/MN_CAP_PRECIOS/IPC_EMP_2023/638415285164039007"
 
+# Extraer y procesar los datos
 dato_0 <- session(url) |> 
   read_html() |> 
   html_table()
